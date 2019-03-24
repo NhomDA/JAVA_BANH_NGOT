d819d55073fe50b590be7b430693d69f527c49cc
dasksai <huuviet7315@gmail.com>
2019-03-24 18:02:08 +0700

    kết nối sql
    database

--- QL_BanhNgot/src/Database/database.java
+++ QL_BanhNgot/src/Database/database.java
@@ -5,10 +5,54 @@
  */
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author ASUS
  */
 public class database {
-    
+    String connectionString = "jdbc:sqlserver://127.0.0.1:1433;databaseName=QLBanhNgot;user=dasksai;password=manakilosi";
+    Connection conn;
+    public database(){
+       
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(connectionString);
+            
+            if(conn != null){
+                System.out.println("Kết nối CSDL SQL Server thành công!");
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.toString());
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+}
+      public ResultSet ExcuteQueryGetTable(String cauTruyVanSQL){
+        try {
+            Statement stmt = conn.createStatement();           
+            ResultSet rs = stmt.executeQuery(cauTruyVanSQL);
+            return rs;
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+            
+        return null;
+    }
+    //Thực thi INSERT, DELETE, UPDATE
+    public void ExcuteQueryUpdateDB(String cauTruyVanSQL){
+       
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(cauTruyVanSQL);
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
 }
