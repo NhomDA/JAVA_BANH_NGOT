97883b2fc93a9f9ec3342bbb1ac2c7f11dbe9174
dasksai <huuviet7315@gmail.com>
2019-03-25 14:12:41 +0700

    thay doi ten paket
    thay doi

--- QL_BanhNgot/src/Controller/TruyVan.java
+++ QL_BanhNgot/src/Controller/TruyVan.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package Controller;
 
-import Database.database;
+import Model.database;
 import java.sql.ResultSet;
 /**
  *
