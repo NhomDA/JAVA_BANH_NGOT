2dba485b61438c90f01b9382ec79a97ba4a53a6c
dasksai <huuviet7315@gmail.com>
2019-04-03 15:24:50 +0700

    frmQuanLy
    form quan ly

--- QL_BanhNgot/src/Controller/TruyVan.java
+++ QL_BanhNgot/src/Controller/TruyVan.java
@@ -23,7 +23,31 @@ public class TruyVan {
         return db.ExcuteQueryGetTable(sql);
     }
     public ResultSet NhanVien(){
-            String sql = "select * from NhanVien";
-            return db.ExcuteQueryGetTable(sql);
-        }
+        String sql = "select * from NhanVien";
+        return db.ExcuteQueryGetTable(sql);
+    }
+    public ResultSet ListTaiKhoan(){
+        String sql = "select TK, MK, a.MaPQ, TenPQ from DangNhap a, PhanQuyen b where a.MaPQ = b.MaPQ";
+        return db.ExcuteQueryGetTable(sql);
+    }
+    public ResultSet ListTaiKhoan(String tk){
+        String sql = "select TK, MK, a.MaPQ, TenPQ from DangNhap a, PhanQuyen b where a.MaPQ = b.MaPQ and TK like N'"+tk+"%'";
+        return db.ExcuteQueryGetTable(sql);
+    }
+    public ResultSet Quyen(){
+        String sql = "select * from PhanQuyen";
+        return db.ExcuteQueryGetTable(sql);
+    }
+    public void ThemTK( String TK,String MK,int quyen) {         
+        String sql = "insert into DangNhap(TK,MK,MaPQ) values ('"+TK+"','"+MK+"',"+quyen+")";
+        db.ExcuteQueryUpdateDB(sql);	
+    }
+    public void SuaTK( String TK, String MK,int quyen) {         
+        String sql = "update DangNhap set MK= '"+MK+"',MaPQ= "+quyen+" where TK='"+TK+"'";
+        db.ExcuteQueryUpdateDB(sql);	
+    }
+    public void XoaTK(String TK) {	
+        String sql = "delete from DangNhap where TK='"+TK+"'";
+        db.ExcuteQueryUpdateDB(sql);	
+    }
 }
